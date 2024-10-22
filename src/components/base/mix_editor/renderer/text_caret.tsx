c0c66451df11b908b7c057bf6cf560eba13f2829
Suilta Pico <256329717@qq.com>
2024-10-22 22:10:28 +0800

    新增：添加了文本光标的状态

--- src/components/base/mix_editor/renderer/text_caret.tsx
+++ src/components/base/mix_editor/renderer/text_caret.tsx
@@ -1,3 +1,12 @@
-export const TextCaret = () => {
-  return <div class="__text_caret"></div>;
+import { createSignal } from "@/common/signal";
+import { Component } from "solid-js";
+
+export class TextCaret {
+  show = createSignal<boolean>(false);
+  constructor() {}
+}
+
+export const TextCaretRenderer: Component<{ state: TextCaret }> = (props) => {
+  const show = props.state.show;
+  return show.get() && <div class="__text_caret"></div>;
 };
